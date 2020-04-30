6f83fd373ecc6c0c977920568597f396c6585315
Yousef Majeed <uaso0of056@gmail.com>
2020-04-30 11:28:00 +0300

    attach addEntry comp to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
+import AddEntry from "./components/AddEntry";
+
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<View>
 			{/* <Text>Open Yousef App.tsx to start working on your app!</Text> */}
-			<Ionicons name="ios-pizza" color="red" size={100} />
+			{/* <Ionicons name="ios-pizza" color="red" size={100} /> */}
+			<AddEntry></AddEntry>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: "#fff",
+// 		alignItems: "center",
+// 		justifyContent: "center",
+// 	},
+// });
